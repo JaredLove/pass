324aada6a1021b9824cb51ec5eefc7cf7fce4915
JaredLove <lovejared91@Yahoo.com>
2024-06-28 18:25:18 -0500

    added prompt for length of password and error checking

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const numeric = ['1','2','3','4','5','6','7','8','9'];
+const special = ['!','@','#','$','%','^','&','*','(',')','_','+'];
+
+const generatePassword = function(){
+  let length = window.prompt("How long would you like your password to be between 8-128 characters long?");
+  console.log(length);
+  if(isNaN(length) || length === null || length === undefined ){
+    window.alert("Please type a number between 8 and 128!");
+    generatePassword();
+  }else if(length < 8 || length > 128){
+    window.alert("Please type a number between 8 and 128!");
+    generatePassword();
+  }
+
+  const nerArr = [];
+  let lowerPrompt
+  let upperPrompt
+  let numericPrompt
+  let specialPrompt
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
